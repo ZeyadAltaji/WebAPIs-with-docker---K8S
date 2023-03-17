e552a90533056f4e6b678711a3fd78edf9722a50
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-17 20:29:14 +0300

    building model

---
+++ PlatformService/Model/Platform.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Model
+{
+    public class platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public string  Cost { get; set; }
+    }
+}
