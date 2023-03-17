59acf86162c4729c2d3857275de0205f5d344147
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-17 22:07:53 +0300

    create new class prepDb

---
+++ PlatformService/Data/PreoDb.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+
+namespace PlatformService.Data
+{
+    public static class PreoDB
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+
+        }
+
+    }
+}
