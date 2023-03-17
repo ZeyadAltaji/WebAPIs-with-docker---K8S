fcf08a85564b0c5f603f087d9d8a37606b2f1caa
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-17 21:47:50 +0300

    create class Db Context

---
+++ PlatformService/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Model;
+
+namespace PlatformService.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> option):base(option)
+        {
+            
+        }
+        public DbSet<platform> Platforms{get;set;}
+
+    }
+}
