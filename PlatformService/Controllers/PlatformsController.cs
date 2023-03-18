b2633a58c4716f403feb6666c1ac65ff5371c6e2
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-18 10:45:46 +0300

    create RestApi Get By Id & create new platForms

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.DTOs;
+using PlatformService.Model;
 
 namespace PlatformService.Controllers
 {
@@ -27,6 +28,25 @@ namespace PlatformService.Controllers
             var platformItem = _repo.GetAllPlatforms();
 
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+        [HttpGet("{id}",Name="GetPlatFormsById")]
+        public ActionResult<PlatformReadDto>GetPlatFormsById(int id)
+        {
+            var platformItem =_repo.GetPlatFormsById(id);
+            if(platformItem !=null){
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult<PlatformReadDto>CreatePlatforms(PlatformCreateDto platformCreateDto)
+        {
+            var platfromModel =_mapper.Map<Platform>(platformCreateDto);
+            _repo.CreatePlatForms(platfromModel);
+            _repo.SaveChanges();
+            var PlatformReadDTO =_mapper.Map<PlatformReadDto>(platfromModel);
+            return CreatedAtAction(nameof(GetPlatFormsById),new {Id =PlatformReadDTO.Id},PlatformReadDTO);
+
         }
     }
 }
