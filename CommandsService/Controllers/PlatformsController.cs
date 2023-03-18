d758b448ade3a32a72b3beaaf7c7b3b4427ac158
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-18 19:03:07 +0300

    add file using run into command servies

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,10 +1,23 @@
- using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Mvc;
  namespace CommandsService.Controllers
 {
-    [Route("api/[Controller]")]
+    [Route("api/commands/[Controller]")]
     [ApiController]
     public class PlatformsController : ControllerBase
     {
+        public PlatformsController()
+        {
+
+
+        }
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+               Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controler");
+        }
 
     }
 }
