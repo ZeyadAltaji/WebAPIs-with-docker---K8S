f8aff645a5d82db98bf5237f5f212d5c2a1814cf
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-26 01:06:04 +0300

    add sql server with EF

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -19,20 +19,33 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        private readonly IHostEnvironment _env;
+        public Startup(IConfiguration configuration,IHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
-        public IConfiguration Configuration { get; }
+      
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDBContext>(option=>option.UseInMemoryDatabase("InMemory"));
+            if(_env.IsProduction())
+            {
+                Console.WriteLine("--> Using Sql Server DB !");
+                services.AddDbContext<AppDBContext>(option=>option.UseSqlServer(Configuration.GetConnectionString("PlatformsConnection")));
+            }
+            else
+            {
+                Console.WriteLine("--> Using InMemory DB !");
+                services.AddDbContext<AppDBContext>(option=>option.UseInMemoryDatabase("InMemory"));
+            }
             services.AddScoped<IPlatformReop,PlatformReop>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            services.AddHttpClient<ICommandDataClient,HttpCommandDataClient>();            services.AddControllers();
+            services.AddHttpClient<ICommandDataClient,HttpCommandDataClient>(); 
+            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
@@ -60,7 +73,7 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
-            PreoDB.PrepPopulation(app);
+            PreoDB.PrepPopulation(app,env.IsProduction());
         }
     }
 }
