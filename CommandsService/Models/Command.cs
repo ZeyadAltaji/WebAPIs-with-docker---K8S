22dc790fccaf36966494326db1c72ce5f66f8eba
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-26 16:20:36 +0300

    create models Command

---
+++ CommandsService/Models/Command.cs
@@ -0,0 +1,10 @@
+namespace CommandsService.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        public string  HowTo { get; set; }
+        public string CommandLine { get; set; }
+        public int PlatFromsId { get; set; }
+    }
+}
