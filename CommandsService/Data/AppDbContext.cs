bf0b5def91342bf5975f00ecddf1a00010c273ad
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-26 21:55:22 +0300

    change name class

---
+++ CommandsService/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+using CommandsService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandsService.Data
+{
+    public class AppDbContext:DbContext
+    {
+         public AppDbContext(DbContextOptions<AppDbContext>option):base(option){}
+        public DbSet<Command>commands{get;set;}
+        public DbSet<PlatFroms>platfroms{get;set;}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+                modelBuilder
+                .Entity<PlatFroms>()
+                .HasMany(p=>p.Commands)
+                .WithOne(p=>p.platFroms!)
+                .HasForeignKey(p=>p.PlatFromsId);
+                modelBuilder
+                .Entity<Command>()
+                .HasOne(p=>p.platFroms)
+                .WithMany(p=>p.Commands)
+                .HasForeignKey(p=>p.PlatFromsId);                
+        }
+    }
+}
