36bff3927ae9df1d5682b89478855f9feba101fd
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-26 16:19:56 +0300

    create models platfroms

---
+++ CommandsService/Models/PlatFroms.cs
@@ -0,0 +1,10 @@
+namespace CommandsService.Models
+{
+    public class PlatFroms
+    {
+        public int Id { get; set; }
+        public int ExternalID { get; set; }
+        public string Name { get; set; }
+    }
+    
+}
