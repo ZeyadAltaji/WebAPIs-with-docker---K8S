fdb35d036f6a4343f04ce4a13b38346ed3bed69d
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-26 16:44:37 +0300

    Add Att with mode

--- CommandsService/Models/PlatFroms.cs
+++ CommandsService/Models/PlatFroms.cs
@@ -1,10 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace CommandsService.Models
 {
     public class PlatFroms
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public int ExternalID { get; set; }
+        [Required]
         public string Name { get; set; }
+        public ICollection<Command> Commands{get;set;}=new List<Command>();
+
+      
     }
     
 }
